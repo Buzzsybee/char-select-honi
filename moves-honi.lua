591066d40daf76170cf4f59872551c5d10d28883
Honii <moonhrtt@gmail.com>
2025-05-24 14:50:57 -0500

    i still dunno what im doing
    some placeholder for honi update thing, added spinn to the twirl(hopefully), also check for landing and the b button dive thing, im melting

--- moves-honi.lua
+++ moves-honi.lua
@@ -18,7 +18,12 @@ for i = 0x (MAX_PLAYERS - 1) do
     reset_honi_states(i)
 end
 
-local function update_honi_speed()
+local function update_honi(m)
+    --idunno what update functions do yett... placeholder for when i do tho
+end
+
+--[[
+local function update_honi_speed(m)
     local targSpeedMax;
     local targSpeed;
 
@@ -34,6 +39,9 @@ local function update_honi_speed()
     apply_slope_accel(m);
 end
 
+Squishy did something like this for walking speed, i dont really know how it works, i might ask her :3c
+]]--
+
 ACT_HONI_TWIRL     = allocate_mario_action(ACT_GROUP_AIRBORNE|ACT_FLAG_AIR|ACT_FLAG_ATTACKING)
 ACT_HONI_DRILL     = allocate_mario_action(ACT_GROUP_MOVING|ACT_FLAG_ATTACKING)
 ACT_HONI_DIVE      = allocate_mario_action(ACT_GROUP_MOVING|ACT_FLAG_ATTACKING|ACT_FLAG_DIVING)
@@ -43,6 +51,7 @@ ACT_HONI_AIR_DASH  = allocate_mario_action(ACT_GROUP_AIRBORNE|ACT_FLAG_AIR|ACT_F
 --- @param m MarioState
 local function honi_twirl(m)
     local e = gExtrasStates[m.playerIndex]
+    local mag = (m.controller.stickMag) / 64
     if m.actionTimer == 0 then
         set_mario_animation(m, CHAR_ANIM_TWIRLING)
         m.vel.y = 20
@@ -50,12 +59,22 @@ local function honi_twirl(m)
         m.forwardVel = 10
     end
     
-    local air = perform_air_step(m)
+    m.marioObj.header.gfx.angle.y = m.marioObj.header.gfx.angle.y + 0x9000 -- this is supposed to make u spin, idunno if it workss
 
-    if (m.controller.buttonPressed & B_BUTTON ~= 0) and e.canTwirlAct ~= 0 then
-        m.vel.y = 6
+    --update_honi_speed(m)
+
+    local air = perform_air_step(m)
+    if air == AIR_STEP_LANDED then 
+        if m.forwardVel > 0 then
+            if mag > 0 then
+                set_mario_action(m, ACT_WALKING, 0)
+            end
+        else set_mario_action(m, ACT_IDLE, 0) end
+    end
+    if (m.controller.buttonPressed & B_BUTTON ~= 0) and e.canTwirlAct then
+        m.vel.y = 10
         m.forwardVel = 30
-        set_mario_action(m, ACT_DIVE) -- replace with the airdash when added :3c
+        set_mario_action(m, ACT_DIVE, 0) -- replace with the airdash when added :3c
     end
 
     m.faceAngle.y = approach_s32(convert_s16(m.faceAngle.y), m.intendedYaw, 0x100)
