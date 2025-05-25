4bbed8095d3d875d65424b521781d336adc4e1a6
Honii <moonhrtt@gmail.com>
2025-05-25 13:51:04 -0500

    many many stuff idunno what im doing

--- moves-honi.lua
+++ moves-honi.lua
@@ -5,9 +5,9 @@ function reset_honi_states(index)
     if index == nil then index = 0 end
     gExtrasStates[index] = {
         index = network_global_index_from_local(0),
-        drillVel = 0,
         chargedFling = 0,
         canTwirlAct = true,
+        canTwirl = true,
         twirlFromDive = true,
         airDash = true
     }
@@ -17,33 +17,34 @@ for i = 0x (MAX_PLAYERS - 1) do
     reset_honi_states(i)
 end
 
-local twirlActs = {
+local canTwirlFromAct = {
+    [ACT_DIVE] = true,
     [ACT_JUMPING] = true,
     [ACT_DOUBLE_JUMP] = true,
     [ACT_TRIPLE_JUMP] = true,
-    [ACT_HOLD_JUMP] = true,
-    [ACT_GROUND_POUND] = true,
+    [ACT_HOLD_JUMP] = true
 }
 
 local function honi_on_set_action(m)
     --i also dont know how this works, placeholder fot the moment
     local e = gExtrasStates[m.playerIndex]
-    if m.action == ACT_TWIRLING then
-        set_mario_action(m, ACT_HONI_TWIRL, 0)
-    end
 end
 
 local function update_honi(m)
-    --idunno what update functions do yett... placeholder for when i do tho
+    --idunno what update functions do yett... fully
     local e = gExtrasStates[m.playerIndex]
     if m.action == ACT_HONI_TWIRL then
-        e.canTwirlAct = false
+        e.canTwirl = false -- if already twirling, cant twirl again :3
     end
     if m.pos.y == m.floorHeight then
-        e.canTwirlAct = true
+        e.canTwirl = true -- if touching land, u can twirl again midair.
+        e.airDash = true -- if touching land, u can air dash again.
+    end
+    if m.action == ACT_DIVE then 
+        e.airDash = false
     end
 
-    if m.input & INPUT_A_PRESSED ~= 0 and m.pos.y > m.floorHeight and e.canTwirlAct then
+    if m.input & INPUT_A_PRESSED ~= 0 and m.pos.y > m.floorHeight and e.canTwirl and canTwirlFromAct[m.action] then
         set_mario_action(m, ACT_HONI_TWIRL, 0)
     end
 end
@@ -69,7 +70,6 @@ local function honi_twirl(m)
     
     m.marioObj.header.gfx.angle.y = m.marioObj.header.gfx.angle.y + 0x9000 -- this is supposed to make u spin, idunno if it workss
 
-
     local air = perform_air_step(m)
     if air == AIR_STEP_LANDED then 
         if m.forwardVel > 0 then
@@ -83,12 +83,52 @@ local function honi_twirl(m)
     m.vel.x = m.forwardVel * sins(m.faceAngle.y)
     m.vel.z = m.forwardVel * coss(m.faceAngle.y)
 
+    if m.input & INPUT_A_PRESSED ~= 0 and e.airDash then
+        m.vel.y = 10
+        m.forwardVel = 30
+        set_mario_action(m, ACT_DIVE, 0)
+    end
+
     m.actionTimer = m.actionTimer + 1
 end
 
+local function honi_drill(m)
+    local e = gExtrasStates[m.playerIndex] 
+    local mag = (m.controller.stickMag) / 64
+
+    set_mario_animation(m, CHAR_ANIM_TWIRLING)
+
+    if m.actionTimer == 0 then
+        m.faceAngle.y = m.intendedYaw
+    end
+
+    if mag > 0 then -- this is supposed to check if ur holding the stick, but i dont knoo
+        if m.forwardVel < 30 then m.forwardVel = 30 end
+        m.forwardVel = m.forwardVel + (mag * 5)
+        if m.forwardVel > 50 the m.forwardVel = 50 end
+    elseif mag == 0 then -- and this is if ur not holding stickk
+        m.forwardVel = m.forwardVel - 2
+    end
+
+    m.marioObj.header.gfx.angle.y = m.marioObj.header.gfx.angle.y + 0x9999
+
+    m.faceAngle.y = approach_s32(convert_s16(m.faceAngle.y), m.intendedYaw, 0x400)
+    m.vel.x = m.forwardVel * sins(m.faceAngle.y)
+    m.vel.z = m.forwardVel * coss(m.faceAngle.y)
+
+    local ground = perform_ground_step(m)
+
+    if ground == GROUND_STEP_LEFT_GROUND then
+        set_mario_action(m, ACT_FREE_FALL, 0)
+    end
+
+    m.actionTimer = m.actioTimer + 1
+end
+
 hook_mario_action(ACT_HONI_TWIRL,{every_frame = honi_twirl})
+hook_mario_action(ACT_HONI_DRILL,{every_frame = honi_drill})
+
 
 _G.charSelect.character_hook_moveset(CHAR_HONI, HOOK_MARIO_UPDATE, update_honi)
 _G.charSelect.character_hook_moveset(CHAR_HONI, HOOK_ON_LEVEL_INIT, reset_honi_states)
 _G.charSelect.character_hook_moveset(CHAR_HONI, HOOK_ON_SET_MARIO_ACTION, honi_on_set_action)
-
