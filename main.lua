e0519a80c95a77d2ad807f02efcb6d23d064d239
Honii <moonhrtt@gmail.com>
2025-05-25 11:02:37 -0500

    properly set the character now i think X3

--- main.lua
+++ main.lua
@@ -9,16 +9,14 @@ if not _G.charSelectExists then
     return 0
 end
 
-local function on_character_select_load()
-    CHAR_HONI = _G.charSelect.character_add(
-        "Honi", -- Character Name
-        "Just a floaty gal, would u believe ittt?!?!?!?!?!?!", -- Description
-        "Honi", -- Credits
-        "03fc7b",           -- Menu Color
-        E_MODEL_MARIO,       -- Character Model
-        CT_MARIO,           -- Override Character
-        TEX_CHAR_LIFE_ICON, -- Life Icon
-        1,                  -- Camera Scale
-        0                   -- Vertical Offset
-    )
-end
+CHAR_HONI = _G.charSelect.character_add(
+    "Honi", -- Character Name
+    "Just a floaty gal, would u believe ittt?!?!?!?!?!?!", -- Description
+    "Honi", -- Credits
+    "03fc7b",           -- Menu Color
+    E_MODEL_MARIO,       -- Character Model
+    CT_MARIO,           -- Override Character
+    TEX_CHAR_LIFE_ICON, -- Life Icon
+    1,                  -- Camera Scale
+    0                   -- Vertical Offset
+)
